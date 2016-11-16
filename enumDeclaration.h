c87e9972445d237fc0c5ad96f992f348e83c1adc
jmalvey5459 <john.malvey@mymail.champlain.edu>
2016-11-16 12:31:40 -0500

    enumDeclaration.h
    mr enum guy pulling through with the enums

---
+++ enumDeclaration.h
@@ -0,0 +1,14 @@
+//Enums by Mr Enum Guy AKA Jack Malvey
+
+//Include this header in any file that needs to use the enums
+
+//all variables start at 0 and go up by one
+
+//Ex: crew = 0, first = 1, second = 2, third = 3
+enum SocialClass{Crew,First,Second,Third};
+
+enum Gender{Male,Female};
+
+enum Age{Child,Adult};
+
+enum Survival{No, Yes};
